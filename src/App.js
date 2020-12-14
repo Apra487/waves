37419655aa960b304f32c166f00a5af55aa15e77
Apratim Mahata <apra487@gmail.com>
2020-12-14 17:30:00 +0530

    implementing debouncing for keyboard event

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import debounce from 'lodash.debounce';
 import Song from './components/Song';
 import Player from './components/Player';
 import Library from './components/Library';
@@ -7,19 +8,36 @@ import './styles/app.scss';
 import data from './util';
 
 function App() {
-	useEffect(() => {
-		window.addEventListener('keydown', (event) => {
-			event.preventDefault();
-			if (event.code === 'Space')
-				isPlaying ? setIsPlaying(false) : setIsPlaying(true);
-		});
-	});
 
 	// * State
 	const [song] = useState(data());
 	const [currentSong, setCurrentSong] = useState(song[0]);
-	const [isPlaying, setIsPlaying] = useState(false);
+	let [isPlaying, setIsPlaying] = useState(false);
 	const [libraryStatus, setLibraryStatus] = useState(false);
+
+	useEffect(() => {
+		window.addEventListener('keydown', (event) => {
+			handleChange(event);
+		});
+	});
+
+	const handleChange = (event) => {
+		if (event.code === 'Space') debouncedSave();
+	};
+
+
+	function playHandle() {
+		isPlaying = isPlaying ? false : true;
+		setIsPlaying(isPlaying);
+	}
+	
+	const debouncedSave = useCallback(
+		debounce(
+			playHandle,
+			400
+		),
+		[]
+	);
 	return (
 		<div className={`App ${libraryStatus ? 'library-active' : ''}`}>
 			<Nav
